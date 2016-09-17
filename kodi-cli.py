8d75f33f28d10a10229518b75e6e38ad35ec2021
Kyle Anderson <kyle@xkyle.com>
2016-09-17 16:59:33 -0700

    Added more more key bindings for webui parity

--- kodi-cli.py
+++ kodi-cli.py
@@ -128,20 +128,22 @@ def main_window(switch_window=None):
        screen.clear()
        screen.keypad(1)
        screen.border(0)
-       screen.addstr(2, 2, "Move... (Arrow keys also work for navigation)")
-       screen.addstr(4, 4, "w: Up")
-       screen.addstr(5, 4, "a: Left")
-       screen.addstr(6, 4, "s: Down")
-       screen.addstr(7, 4, "d: Right")
-       screen.addstr(8, 4, "g/return: Select")
-       screen.addstr(9, 4, "b or q: Back")
-       screen.addstr(10, 4, "h: Home")
+       screen.addstr(2, 2,  "Navigation...")
+       screen.addstr(4, 4,  "w or up: Up")
+       screen.addstr(5, 4,  "a or right: Left")
+       screen.addstr(6, 4,  "s or down: Down")
+       screen.addstr(7, 4,  "d or right: Right")
+       screen.addstr(8, 4,  "g or return: Select")
+       screen.addstr(9, 4,  "b or q or backspace: Back")
+       screen.addstr(10, 4, "h or escape: Home")
        screen.addstr(11, 4, "m: Context Menu")
        screen.addstr(12, 4, "L: List Addons")
        screen.addstr(2, 54, "------------ Media Controls ---------- ")
-       screen.addstr(4, 54, "p: Play/Pause")
+       screen.addstr(4, 54, "p or spacebar: Play/Pause")
        screen.addstr(5, 54, "f: Full Screen")
        screen.addstr(6, 54, "X: Stop")
+       screen.addstr(7, 54, "-: Volume Down")
+       screen.addstr(8, 54, "+: Volume Up")
 
        screen.addstr(14, 4, "Q: Exit")
 
@@ -150,39 +152,44 @@ def main_window(switch_window=None):
        x = screen.getch()
 
        if x == ord('w') or x == curses.KEY_UP:
-        send_input_command("Input.Up")
+           send_input_command("Input.Up")
 
 
        if x == ord('a') or x == curses.KEY_LEFT:
-        send_input_command("Input.Left")
+           send_input_command("Input.Left")
    
        if x == ord('s') or x == curses.KEY_DOWN:
-        send_input_command("Input.Down")
+           send_input_command("Input.Down")
 
        if x == ord('d') or x == curses.KEY_RIGHT:
-         send_input_command("Input.Right")
+           send_input_command("Input.Right")
 
        if x == ord('g') or x == 10:
-         send_input_command("Input.Select")
+           send_input_command("Input.Select")
        
-       if x == ord('h'):
-         send_input_command("Input.Home")
+       if x == ord('h') or x == 27:
+           send_input_command("Input.Home")
 
        if x == ord('m'):
-         send_input_command("Input.ContextMenu")
+           send_input_command("Input.ContextMenu")
 
-       if x == ord('b') or x == ord('q'):
-         send_input_command("Input.Back")
+       if x == ord('b') or x == ord('q') or x == curses.KEY_BACKSPACE:
+           send_input_command("Input.Back")
        
-       if x == ord('p'):
-         send_player_command("Player.PlayPause", playerid=0)
+       if x == ord('p') or x == ord(' '):
+           send_player_command("Player.PlayPause", playerid=0)
 
        if x == ord('f'):
-         send_player_command("GUI.SetFullscreen", fullscreen=True)
+           send_player_command("GUI.SetFullscreen", fullscreen=True)
 
        if x == ord('X'):
-         send_player_command("Player.Stop", playerid=0)
+           send_player_command("Player.Stop", playerid=0)
 
+       if x == ord('-') or x == ord('_'):
+           send_player_command("Application.SetVolume", volume='decrement')
+
+       if x == ord('+') or x == ord('='):
+           send_player_command("Application.SetVolume", volume='increment')
 
        if x == ord('L'):
            switch_window = {'next': 'addons_list',
